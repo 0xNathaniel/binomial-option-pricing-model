36e4c9528c79060794d6a2cf8864d6482e8485ce
Nathaniel Jonathan Rusli <omgitsnathaniels@gmail.com>
2025-06-24 16:40:42 +0700

    feat: add discrete steps sensitivity test visualization

--- src/discrete_steps_test.py
+++ src/discrete_steps_test.py
@@ -1,4 +1,5 @@
 from binomial_model import binomial_american_call
+from visualization import plot_european_price_vs_n
 
 # Variables for Modeling (AAPL Case)
 
@@ -8,17 +9,26 @@ VOLATILITY      = 0.4110088081144711    # Utilize preprocessed variable calculat
 RISK_FREE_RATE  = 0.04273               # Jun 24, 2025 US 3 Month Treasury Yield
 T               = 178/365               # Desired time to maturity (in years)
 DIVIDEND_YIELD  = 0.005012406947890819  # Dividend yield
-N               = [10, 100, 1000]       # List of tested N
+N               = [10,                  # List of tested N
+                   50,
+                   100,
+                   250,
+                   500, 
+                   1000,
+                   2500,
+                   5000]       
 
 # Premium price as of 23 June 2025 is $2.65 (market reference)
 
 if __name__ == "__main__":
+    european_prices = []
     for n in N:
-        # Call the function and print results
         print(f"Number of discrete steps (N): {n}")
         result = binomial_american_call(
             SPOT_PRICE, STRIKE_PRICE, VOLATILITY, RISK_FREE_RATE, T, n, DIVIDEND_YIELD)
         european_price, american_price, early_ex_val, exercise_map = result
+        european_prices.append(european_price)
         print(f"European Call Option Price: {european_price:.4f}")
-        print(f"American Call Option Price: {american_price:.4f}")
         print(f"Difference (Early Exercise Value): {early_ex_val:.4f}\n")
+    # Visualize plot
+    plot_european_price_vs_n(N, european_prices, save_path="../data/visualization_output/visualization_AAPL.png")
